67a21c0945d103ae01ccabc2a4298bbe7f707e99
Dandara P Dias <pessoadiasdandara@gmail.com>
2026-05-08 10:34:11 -0300

    Arquivos de configuração de SO.

---
+++ nono.py
@@ -0,0 +1,107 @@
+#Exercício 1 
+# idade=input("Digite sua idade:")
+# if idade >= 18:
+#    print("Voce é maior de idade. ")
+#correção 
+# idade=int(input("Digite sua idade "))
+# if idade >= 18:
+#    print("Voce é maior de idade")
+# else:
+#     print("Voce é manor de idade ")
+#Exercício 2
+# nome="Mariana"
+# print("Seja bem-vinda,nome!")
+#Correção
+# nome="Mariana"
+# print("Seja bem-vinda",nome)
+
+#Exercício 3
+# numero = 10
+# if numero >5:
+# print("O numero é maior que cinco")
+# else:
+# print("O numero é menor ou igual a cinco.")
+# correção
+# numero= 10 
+# if numero >=5:
+#     print("O numero é maior que cinco")
+# else:
+#     print("O numero é menor ou igual a cinco")
+
+#Exercício 4
+# usuario="aluno123"
+# if usuario =="aluno123"
+#   print("Login realizado com sucesso")
+#Correção 
+# usuario="aluno123"
+# if usuario == "aluno123": 
+#     print("Login realizado com sucesso")
+
+#Exercício 5 
+# clima = "ensolarado"
+# if clima = "chuvoso":
+#   print("Leve um guarda-chuva!")
+#Correção
+# clima = "ensolarado"
+# if clima =="chuvoso":
+#   print("Leve um guarda chuva")
+
+#Exercício 6 
+# pontos = 50 
+# print("Parabens!Voce fez"+pontos+"pontos.")
+#correção 
+# pontos = 50 
+# print("Parabens!Voce fez + ", pontos,"pontos.")
+
+#Exercício 7
+# nota =9.5
+# if nota>=7:
+#     print("Aprovado")
+# elif nota >=9:
+#     print("Exelente!")
+#Correção 
+# nota =float(input("Qual sua nota?"))
+# if nota <=8:
+#     print("Aprovado")
+# elif nota >=9:
+#     print("Exelente!")
+
+#Exercício 8
+# for i in range(5):
+#     print(i)
+#correção 
+# for i in range(1,6):
+#     print(i)
+
+#Exercício 9 
+# tentativas = 1 
+# while tentativas<=3:
+#     print("Tentando conectar...")
+#correção 
+# tentativas = 1
+# while tentativas <=3: 
+#     tentativas += 1
+#     print("Tentando conectar...") 
+
+#Exercício 10
+# while senha =="python123": 
+#   senha=input("Digite a senha secreta:")
+# print("Acesso concedido!")
+#correção
+# senha="python123"
+# while senha !=("python123"):
+#    senha=input("Digite a senha secreta:")
+#    if senha != "python123":
+#       print("Senha incorreta")
+
+# print("Acesso concedido!")
+
+      
+
+
+
+
+
+
+
+    
